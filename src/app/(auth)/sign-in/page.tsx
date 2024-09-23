a81966c8a539f7fcf35124afa7b133244273e39e
sakjanonkk <sakjanon.k@kkumail.com>
2024-09-23 14:39:51 +0700

    [+] make ui for login and sign up not done yet need to make function to contact to databases

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,13 @@
+
+import SignInForm from '@/components/form/SignInForm'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className=' w-full'>
+      <SignInForm />
+    </div>
+  )
+}
+
+export default page
