b5a073d3a37e4fb4b8cf1248f4ab16c243095964
sakjanonkk <sakjanon.k@kkumail.com>
2024-09-24 12:56:14 +0700

    [+] call api from dbs to collect user sign up sign in not finish sign out button

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import Link from 'next/link';
-import Image from 'next/image';
-import { buttonVariants } from './ui/button';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button, buttonVariants } from "./ui/button";
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getServerSession(authOptions);
+
   return (
     <div className="bg-zinc-100 py-2 border-b border-s-zinc-200 fixed w-full z-10 top-0">
       <div className="container flex items-center justify-between">
@@ -11,9 +15,13 @@ const Navbar = () => {
           {/* Use direct path to image in public folder */}
           <Image src="/blacklogo.png" alt="Logo" width={100} height={100} />
         </Link>
-        <Link href="/sign-in" className={buttonVariants()}>
-          Sign in
-        </Link>
+        {session?.user ? (
+          <Button variant="destructive">Sign Out</Button>
+        ) : (
+          <Link href="/sign-in" className={buttonVariants()}>
+            Sign in
+          </Link>
+        )}
       </div>
     </div>
   );
