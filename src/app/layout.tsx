85579aa4293e4574d2476fe078e383e20d572212
sakjanonkk <sakjanon.k@kkumail.com>
2024-09-25 21:27:22 +0700

    [+] Google Auth [Backend]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,22 +5,16 @@ import { IBM_Plex_Sans_Thai } from "next/font/google";
 import { Toaster } from "@/components/ui/toaster";
 import Provider from "@/components/Provider";
 
-const ibmPlexSans = IBM_Plex_Sans_Thai({
+const ibmPlexSansThai = IBM_Plex_Sans_Thai({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700"],
   style: ["normal"],
 });
 
-// const poppins = Poppins({
-//   subsets: ["latin"],
-//   weight: ["100", "200", "300", "400", "500", "600", "700"],
-//   style: ["normal", "italic"],
-// });
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className="ibmPlexsan.classname">
-      <body className={ibmPlexSans.className}>
+    <html lang="en" className="ibmPlexsansThai.classname">
+      <body className={ibmPlexSansThai.className}>
         <Provider>
           <main className="h-screen flex flex-col justify-center items-center">
             <Navbar />
