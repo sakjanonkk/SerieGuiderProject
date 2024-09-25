2a4d14f99398106a61caa3e916f42f62621491eb
Nathachai_tn <119283041+Trainght@users.noreply.github.com>
2024-09-25 15:52:24 +0700

    add new all file

---
+++ src/components/form/SignInForm.tsx
@@ -0,0 +1,116 @@
+"use client";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import Link from "next/link";
+import GoogleSignInButton from "../GoogleSignInButton";
+import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+// import Email from "next-auth/providers/email";
+// import { sign } from "crypto";
+
+const FormSchema = z.object({
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must be at least 8 characters"),
+});
+
+const SignInForm = () => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof FormSchema>) => {
+    const signInData = await signIn("credentials", {
+      email: values.email,
+      password: values.password,
+      redirect: false,
+    });
+    // alert(JSON.stringify({ signInData }));
+
+    if (!signInData || signInData.error) {
+      console.log(signInData?.error);
+      // alert(JSON.stringify(signInData?.error));
+    } else {
+      router.push("/admin");
+    }
+  };
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className=" w-full">
+        <div className="space-y-2">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl className="bg-white">
+                  <Input
+                    placeholder="mail@example.com"
+                    type="email"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl className="bg-white">
+                  <Input
+                    type="password"
+                    placeholder="Enter your password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <Button className=" w-full mt-6" type="submit">
+          Submit
+        </Button>
+        <div className="mx-auto my-4 flex w-full items-center justify-evenly before:mr-4 before:block before:h-px before:flex-grow before:bg-stone-400 after:ml-4 after:block after:h-px after:flex-grow after:bg-stone-400">
+          or
+        </div>
+        <GoogleSignInButton> Sign up with Google </GoogleSignInButton>
+
+        {/* <GoogleSignInButton>Sign in with Google</GoogleSignInButton> */}
+        <p className="text-center text-sm text-gray-600 mt-2">
+          If you don&apos;t have an account, please&nbsp;
+          <Link className="text-blue-500 hover:underline" href="/sign-up">
+            Sign up
+          </Link>
+        </p>
+      </form>
+    </Form>
+  );
+};
+
+export default SignInForm;
