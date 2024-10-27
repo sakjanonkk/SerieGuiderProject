f9bd35872748f65b7e80fb54b4921f9c93e697a7
sakjanonkk <sakjanon.k@kkumail.com>
2024-10-28 01:10:00 +0700

    [ ! ] warnning this is not finish yet

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,15 +1,20 @@
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 
 declare module "next-auth" {
   interface User {
-    username: string | null
+    id: string; // Add the ID field to the User interface
+    username: string | null;
   }
+
   interface Session {
     user: User & {
-        username: string
-    }
-    token: {
-        username: string
-    }
+      id: string; // Include the ID field in the session's user object
+      username: string;
+    };
+  }
+
+  interface JWT {
+    id: string; // Add the ID field to the JWT interface
+    username: string;
   }
 }
