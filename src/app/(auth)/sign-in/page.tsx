c8de30ad6266df0052b0b069f0791fcb6dadbd0e
sakjanonkk <sakjanon.k@kkumail.com>
2024-11-01 04:28:05 +0700

    [ + ] ?>

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,13 +1,14 @@
-
-import SignInForm from '@/components/form/SignInForm'
-import React from 'react'
+import SignInForm from "@/components/form/SignInForm";
+import React from "react";
 
 const page = () => {
   return (
-    <div className=' w-full'>
+    <div className=" w-full">
       <SignInForm />
     </div>
-  )
-}
+  );
+  // <div>Hello wrong url :3</div>
+  // );
+};
 
-export default page
+export default page;
